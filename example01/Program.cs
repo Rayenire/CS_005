ad03dd2070c0bc6c53166a699a61884427502eea
Rayenire <aeristarg@gmail.com>
2022-07-12 20:09:20 +0300

    Add example

---
+++ example01/Program.cs
@@ -0,0 +1,94 @@
+﻿// bool Metodi(int chislo)
+// {
+//  bool resultati = false;
+//  if( chislo % 2==0 )
+// { resultati = true;
+//  }
+//  else{resultati = false; }
+//  if (resultati == false)
+// { return false;
+//  } else {
+// return true;
+// }
+// }
+
+
+// bool Metodi(int chislo)
+// {
+//  bool resultati = false;
+//  if (chislo % 2 == 0)
+//  {
+//  resultati = true;
+//  }
+//  else
+//  {
+//  resultati = false;
+//  }
+//  if (resultati == false)
+//  {
+//  return false;
+//  }
+//  else
+//  {
+//  return true;
+//  }
+// }
+
+
+// bool Metodi(int chislo)
+// {
+//  bool resultati = false;
+//  if (chislo % 2 == 0) { resultati = true; }
+//  else { resultati = false; }
+//  if (resultati == false) { return false; }
+//  else { return true; }
+// }
+
+
+// bool Metodi(int chislo)
+// {
+//  bool resultati = false;
+//  if (chislo % 2 == 0) { resultati = true; }
+//  else { resultati = false; }
+//  return resultati;
+// }
+
+
+// bool Metodi(int chislo)
+// {
+//  bool resultati = false;
+//  if (chislo % 2 == 0) { return true; }
+//  else { return false; }
+// }
+
+
+// bool Metodi(int chislo)
+// {
+//  if (chislo % 2 == 0) { return true; }
+//  else { return false; }
+// }
+
+
+// bool Metodi(int chislo)
+// {
+//  if (chislo % 2 == 0) return true;
+//  else return false;
+// }
+
+
+// bool Metodi(int chislo)
+// {
+//  return chislo % 2 == 0);
+// }
+
+
+// bool IsEven(int value)
+// {
+//  return value % 2 == 0;
+// }
+
+
+// bool IsEven(int value) => value % 2 == 0;
+
+
+// var IsEven = (int v) => v % 2 == 0;
